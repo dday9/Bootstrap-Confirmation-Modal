5c62efb983460a52d76b5859099bc05b1aea00ce
David Day <david.day@susco.net>
2024-01-03 08:59:52 -0600

    ShorterFunctions - Refactored v4 so that the buildDom method is broken up into several functions.

--- src/v4/bootstrap-confirmation-modal.js
+++ src/v4/bootstrap-confirmation-modal.js
@@ -1,7 +1,13 @@
 const confirmationModal = {};
 
 confirmationModal.buildDom = configuration => {
-	// optionally setup the default configuration
+	configuration = confirmationModal.setDefaultConfiguration(configuration);
+
+	const modal = confirmationModal.buildModalDom(configuration);
+	return modal;
+};
+
+confirmationModal.setDefaultConfiguration = configuration => {
 	configuration = configuration || {};
 	configuration.title = configuration.title || '';
 	configuration.closeIcon = configuration.closeIcon || false;
@@ -13,70 +19,135 @@ confirmationModal.buildDom = configuration => {
 	configuration.yes.class = (configuration.yes.class || 'btn btn-primary').trim() + ' confirmation-yes';
 	configuration.yes.text = configuration.yes.text || 'Ok';
 
-	// build the required DOM
+	return configuration;
+};
+
+confirmationModal.buildModalDom = configuration => {
 	const modal = document.createElement('div');
 	modal.classList.add('modal');
 	modal.setAttribute('tabindex', '-1');
 
+	const modalDialog = confirmationModal.buildModalDialogDom(configuration);
+	modal.appendChild(modalDialog);
+
+	return modal;
+};
+
+confirmationModal.buildModalDialogDom = configuration => {
 	const modalDialog = document.createElement('div');
 	modalDialog.classList.add('modal-dialog');
 
+	const modalContent = confirmationModal.buildModalContentDom(configuration);
+	modalDialog.appendChild(modalContent);
+
+	return modalDialog;
+};
+
+confirmationModal.buildModalContentDom = configuration => {
 	const modalContent = document.createElement('div');
 	modalContent.classList.add('modal-content');
 
+	if (configuration.title || configuration.closeIcon) {
+		const modalHeader = confirmationModal.buildModalHeaderDom(configuration);
+		modalContent.appendChild(modalHeader);
+	}
+
+	const modalBody = confirmationModal.buildModalBodyDom(configuration);
+	modalContent.appendChild(modalBody);
+
+	const modalFooter = confirmationModal.buildModalFooterDom(configuration);
+	modalContent.appendChild(modalFooter);
+
+	return modalContent;
+};
+
+confirmationModal.buildModalBodyDom = configuration => {
 	const modalBody = document.createElement('div');
 	modalBody.classList.add('modal-body');
 
+	const modalBodyText = confirmationModal.buildModalBodyTextDom(configuration);
+	modalBody.appendChild(modalBodyText);
+	
+	return modalBody;
+};
+
+confirmationModal.buildModalBodyTextDom = configuration => {
 	const modalBodyText = document.createElement('p');
 	modalBodyText.textContent = configuration.message;
-	modalBody.appendChild(modalBodyText);
 
+	return modalBodyText;
+};
+
+confirmationModal.buildModalFooterDom = configuration => {
 	const modalFooter = document.createElement('div');
 	modalFooter.classList.add('modal-footer');
 
+	const modalFooterNoButton = confirmationModal.buildModalFooterNoButtonDom(configuration);
+	modalFooter.appendChild(modalFooterNoButton);
+
+	const modalFooterYesButton = confirmationModal.buildModalFooterYesButtonDom(configuration);
+	modalFooter.appendChild(modalFooterYesButton);
+
+	return modalFooter;
+};
+
+confirmationModal.buildModalFooterNoButtonDom = configuration => {
 	const modalFooterNoButton = document.createElement('button');
 	modalFooterNoButton.setAttribute('class', configuration.no.class);
 	modalFooterNoButton.textContent = configuration.no.text;
 	modalFooterNoButton.dataset.dismiss = 'modal';
+	
+	return modalFooterNoButton;
+};
 
+confirmationModal.buildModalFooterYesButtonDom = configuration => {
 	const modalFooterYesButton = document.createElement('button');
 	modalFooterYesButton.setAttribute('class', configuration.yes.class);
 	modalFooterYesButton.textContent = configuration.yes.text;
+	
+	return modalFooterYesButton;
+};
 
-	modalFooter.appendChild(modalFooterNoButton);
-	modalFooter.appendChild(modalFooterYesButton);
-	modalContent.appendChild(modalBody);
-	modalContent.appendChild(modalFooter);
+confirmationModal.buildModalHeaderDom = configuration => {
+	const modalHeader = document.createElement('div');
+	modalHeader.classList.add('modal-header');
 
-	// optionally build the header DOM
-	if (configuration.title || configuration.closeIcon) {
-		const modalHeader = document.createElement('div');
-		modalHeader.classList.add('modal-header');
+	const modalTitle = confirmationModal.buildModalTitleDom(configuration);
+	modalHeader.appendChild(modalTitle);
 
-		const modalTitle = document.createElement('h5');
-		modalTitle.classList.add('modal-title');
-		modalTitle.textContent = configuration.title;
-		modalHeader.appendChild(modalTitle);
-
-		// optionally build the close icon button
-		if (configuration.closeIcon) {
-			const closeText = document.createElement('span');
-			closeText.setAttribute('aria-hidden', 'true');
-			closeText.innerHTML = '&times;';
-
-			const closeButton = document.createElement('button');
-			closeButton.classList.add('close');
-			closeButton.dataset.dismiss = 'modal';
-			closeButton.appendChild(closeText);
-			modalHeader.appendChild(closeButton);
-		}
-		modalContent.prepend(modalHeader);
+	if (configuration.closeIcon) {
+		const closeButton = confirmationModal.buildModalCloseDom();
+		modalHeader.appendChild(closeButton);
 	}
 
-	// finish building the DOM and return the modal
-	modalDialog.appendChild(modalContent);
-	modal.appendChild(modalDialog);
-	return modal;
+	return modalHeader;
+};
+
+confirmationModal.buildModalTitleDom = configuration => {
+	const modalTitle = document.createElement('h5');
+	modalTitle.classList.add('modal-title');
+	modalTitle.textContent = configuration.title;
+	
+	return modalTitle;
+};
+
+confirmationModal.buildModalCloseDom = () => {
+	const closeButton = document.createElement('button');
+	closeButton.classList.add('close');
+	closeButton.dataset.dismiss = 'modal';
+
+	const closeText = confirmationModal.buildModalCloseTextDom();
+	closeButton.appendChild(closeText);
+
+	return closeButton;
+};
+
+confirmationModal.buildModalCloseTextDom = () => {
+	const closeText = document.createElement('span');
+	closeText.setAttribute('aria-hidden', 'true');
+	closeText.innerHTML = '&times;';
+
+	return closeText;
 };
 
 confirmationModal.show = params => {
